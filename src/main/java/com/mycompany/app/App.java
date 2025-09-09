ca0a5050b9c4afc1db9b69061d4f93489c44fee7
soniaayush1026 <soniaayush2610@gmail.com>
2025-09-09 17:13:22 +0530

    aap.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,17 +3,27 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
+import java.io.IOException;
+import java.io.OutputStream;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.net.InetSocketAddress;
+
 public class App {
-
-    private static final String MESSAGE = "Hello World!";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(MESSAGE);
-    }
-
-    public String getMessage() {
-        return MESSAGE;
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello World!";
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started at http://localhost:8080");
     }
 }
