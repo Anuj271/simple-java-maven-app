f47c22b74ef8f80f2b9f315ec46ac0a6e982869d
soniaayush1026 <soniaayush2610@gmail.com>
2025-09-09 15:52:46 +0530

    aap.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,20 +1,19 @@
 package com.mycompany.app;
 
-import com.sun.net.httpserver.HttpServer;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
+/**
+ * Hello world!
+ */
 public class App {
-    public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", exchange -> {
-            String response = "Hello World from Java HTTP Server!";
-            exchange.sendResponseHeaders(200, response.length());
-            try (OutputStream os = exchange.getResponseBody()) {
-                os.write(response.getBytes());
-            }
-        });
-        server.start();
-        System.out.println("Server started on port 8080");
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
     }
 }
