5deaa5f46ce3024f3a56225c5bb69b103f806cb9
soniaayush1026 <soniaayush2610@gmail.com>
2025-09-16 20:49:06 +0530

    change the path of service yaml

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import java.net.InetSocketAddress;
 
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello Anuj!";
 
     public App() {}
 
