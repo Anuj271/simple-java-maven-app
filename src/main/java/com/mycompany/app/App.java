bbe7c8291a015886762bc3dce503074014730c75
soniaayush1026 <soniaayush2610@gmail.com>
2025-09-24 22:07:19 +0530

    Changed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import java.net.InetSocketAddress;
 
 public class App {
 
-    private static final String MESSAGE = "Hello Rahul!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
